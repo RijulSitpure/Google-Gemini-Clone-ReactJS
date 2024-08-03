0da1bedb346429bccfbb59e8451f37464edaff47
RijulSitpure <sitpurerijul@gmail.com>
2024-08-04 04:50:03 +0530

    Update Context.jsx

--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -6,8 +6,8 @@ export const Context = createContext();
 const ContextProvider = (props) => {
 
 
-    const onSent = async (prompt) => { 
-        await run(prompt)
+    async function onSent(prompt) {
+        await run(prompt);
     }
 
     onSent("What is react js?")
