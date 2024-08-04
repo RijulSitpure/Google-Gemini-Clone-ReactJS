88df5fd56bea03f0de857fd2f133106c0585e774
RijulSitpure <sitpurerijul@gmail.com>
2024-08-04 12:41:39 +0530

    Final Animations to overall website

--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -20,6 +20,11 @@ const ContextProvider = (props) => {
         },75*index)
 
     }
+    
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
 
     const onSent = async (prompt) => {
 
@@ -27,10 +32,16 @@ const ContextProvider = (props) => {
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        setPrevPrompts(prev=>[...prev,input])
-
-        const response = await run(input)
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt)
+            setRecentPrompt(prompt)
+        }
+        else {
+            setPrevPrompts(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
 
         let responseArray = response.split("**");
         let newResponse = "";
@@ -71,7 +82,8 @@ const ContextProvider = (props) => {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
     }
 
     return (
